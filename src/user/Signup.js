6a0313233b3994686428e7af9145ca96622ca459
millord <juancarlosmillord@gmail.com>
2020-01-06 21:30:44 -0400

    added the form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Layout from "../core/Layout";
 
-const Signup = () => (
-  <Layout
-    title="Signup"
-    description="Signup to Node React E-commerce App Signup"
-  >
-    ...
-  </Layout>
-);
+const Signup = () => {
+  const signUpForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Email</label>
+          <input type="email" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Password</label>
+          <input type="password" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    );
+  };
+  return (
+    <Layout
+      title="Signup"
+      description="Signup to Node React E-commerce App Signup"
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+    </Layout>
+  );
+};
 
 export default Signup;
