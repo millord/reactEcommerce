8a69eb48670548b171564e89cd10abbef1582d67
millord <juancarlosmillord@gmail.com>
2020-01-09 07:52:34 -0400

    fixed cords

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,26 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    succes: false
+  });
+
+  const { name, email, password } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signup = user => {
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    signup(name, email, password);
+  };
+
   const signUpForm = () => {
     return (
       <form>
         <div className="form-group">
           <label className="text-muted">Name</label>
-          <input type="text" className="form-control" />
+          <input
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label className="text-muted">Email</label>
-          <input type="email" className="form-control" />
+          <input
+            onChange={handleChange("email")}
+            type="email"
+            className="form-control"
+          />
         </div>
         <div className="form-group">
           <label className="text-muted">Password</label>
-          <input type="password" className="form-control" />
+          <input
+            onChange={handleChange("password")}
+            type="password"
+            className="form-control"
+          />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button onClick={clickSubmit} className="btn btn-primary">
+          Submit
+        </button>
       </form>
     );
   };
+
   return (
     <Layout
       title="Signup"
@@ -28,6 +80,7 @@ const Signup = () => {
       className="container col-md-8 offset-md-2"
     >
       {signUpForm()}
+      {JSON.stringify(values)}
     </Layout>
   );
 };
