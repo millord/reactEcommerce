fd26c3f79b8de1e05eac9e9cc76d3f4601c5d0e7
millord <juancarlosmillord@gmail.com>
2020-01-19 23:33:01 -0400

    added the addCategory component

---
+++ src/admin/AddCategory.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Layout from "../core/Layout";
+import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
+
+const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const { user, token } = isAuthenticated();
+
+  const handleChange = e => {
+    setError("");
+    setName(e.target.value);
+  };
+
+  const clickSubmit = e => {
+    e.preventDefault();
+    setError("");
+    setSuccess(false);
+  };
+
+  const newCategoryForm = () => (
+    <form onSubmit={clickSubmit}>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          className="form-control"
+          type="text"
+          onChange={handleChange}
+          value={name}
+          autoFocus
+        />
+      </div>
+      <button className="btn btn-outline-primary">Create Category</button>
+    </form>
+  );
+  return (
+    <Layout
+      title="Add a new category"
+      description={`Hi ${name}, ready to add a new category?`}
+    >
+      <div className="row">
+        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AddCategory;
